ff44a27213a515efbdef9f2fcc64c3763c050dee
Daniel Wurring <103683060+reis-daniel@users.noreply.github.com>
2022-04-25 19:24:53 +0200

    loggen yay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 let headline = document.querySelector("h1");
 
 headline.style.color = "blue";
+
+console.log("Works!");
+console.log(3 + 3);
+console.log(3 - 3);
+console.log(3 * 3);
+console.log(3 / 3);
+console.log(7 % 3);
