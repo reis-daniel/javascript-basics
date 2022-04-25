0f4fe3af484294e6b345565da814042e5aec9c11
Daniel Wurring <103683060+reis-daniel@users.noreply.github.com>
2022-04-25 19:42:34 +0200

    javascript added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,25 @@ let headline = document.querySelector("h1");
 
 headline.style.color = "blue";
 
+// Output
 console.log("Works!");
 console.log(3 + 3);
 console.log(3 - 3);
 console.log(3 * 3);
 console.log(3 / 3);
 console.log(7 % 3);
+
+// Variables
+// let & const
+
+// Datatypes
+// string, number and boolean
+
+// Declaration and assigment of a variable
+let firstName = "Freddy";
+let nachName = "der Schöne";
+const number = 33;
+
+// Output of variable
+console.log(firstName);
+console.log(`${firstName} ${nachName}`);
